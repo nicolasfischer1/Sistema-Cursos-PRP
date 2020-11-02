81608803eac9bbe4a6886bc5a347b7671aeae82c
Nicolas Fischer <67649989+nicolasfischer1@users.noreply.github.com>
2020-11-02 20:14:45 -0300

    Atualização e organização das dependências do projeto

--- Bibliotecas/Tipos_Registros.h
+++ Bibliotecas/Tipos_Registros.h
@@ -1,5 +1,5 @@
 /**
- * Arquivo com código-fonte de uma biblioteca: "Var_Types-INTERNAL.h".
+ * Arquivo com código-fonte de uma biblioteca: "Tipos_Registros.h".
  * Definição das estruturas de registro e tipos de variáveis criadas com base nelas.
  * Criado em: 29/10/2020.
  * Última modificação por Nicolas Fischer no dia 29/10/2020.
@@ -8,7 +8,7 @@
 struct aluno //  Estrutura (registro) "aluno"
 {
     int matricula;
-    char nome[30];
+    char nome[60];
     int idade;
     int turma;
 };
@@ -17,11 +17,11 @@ struct turma //   Estrutura (registro) "turma"
 {
     int codigo;
     char curso[60];
-    char turno;
+    char turno; // [M] para manhã; [T] para tarde; [N] para noite
     int qtd_alunos;
     int qtd_limite;
 };
 
 //  Definindo os tipos a partir das estruturas (registros)
-typedef struct aluno aluno;
-typedef struct turma turma;
+typedef struct aluno Aluno;
+typedef struct turma Turma;
---
+++ Main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "Bibliotecas/Tipos_Registros.h"
+#include "Bibliotecas/Manipulacao_Alunos.h"
+#include "Bibliotecas/Manipulacao_Turmas.h"
+
+int main(void)
+{
+
+    return 0;
+}
